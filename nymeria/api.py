028b3f62a0db6c182a8967464330929df4d1453c
Nymeria <dev@nymeria.io>
2022-04-28 09:53:10 -0700

    Added basic check_authentication support

--- nymeria/api.py
+++ nymeria/api.py
@@ -1,9 +1,37 @@
+import urllib3
+import json
+
+def request(endpoint, key='', version='v3'):
+    headers = {
+            'X-Api-Key': key,
+    }
+
+    http = urllib3.PoolManager()
+
+    resp = http.request(
+            'POST',
+            'https://www.nymeria.io/api/{}{}'.format(version, endpoint),
+            headers=headers,
+    )
+
+    return json.loads(resp.data)
+
 class Client:
     def __init__(self, api_key):
         self.key = api_key
 
+    """
+        Check your API Key. If the key is valid, this method returns True.
+        False is returned otherwise.
+    """
     def check_authentication(self):
-        True
+        resp = request('/check-authentication', key=self.key)
+
+        if 'status' in resp:
+            if resp['status'] == 'success':
+                return True
+
+        return False
 
     def enrich(self, *args):
         print(args)
