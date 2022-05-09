8d0e005593e9a3507e65c464620e65eecbe654fd
Nymeria <dev@nymeria.io>
2022-05-09 10:08:14 -0700

    Updated API with search and reveal

--- nymeria/api.py
+++ nymeria/api.py
@@ -1,9 +1,9 @@
 import urllib3
 import json
 
-USER_AGENT = 'nymeria.py/1.0.2'
+USER_AGENT = 'nymeria.py/1.0.3'
 
-def request(endpoint, key='', version='v3', payload=None):
+def request(endpoint, key='', version='v3', payload=None, method='POST', fields=None):
     headers = {
             'User-Agent': USER_AGENT,
             'Content-Type': 'application/json',
@@ -18,10 +18,11 @@ def request(endpoint, key='', version='v3', payload=None):
         body = json.dumps(payload).encode('utf-8')
 
     resp = http.request(
-            'POST',
+            method,
             'https://www.nymeria.io/api/{}{}'.format(version, endpoint),
             headers=headers,
             body=body,
+            fields=fields,
     )
 
     return json.loads(resp.data)
@@ -54,7 +55,7 @@ class Client:
 
             if 'status' in resp and 'data' in resp and 'usage' in resp:
                 if resp['status'] == 'success':
-                    return { 'data': resp['data'], 'usage': resp['usage'] }
+                    return { 'status': 'success', 'data': resp['data'], 'usage': resp['usage'] }
 
             message = 'An unknown error ocurred'
 
@@ -68,7 +69,7 @@ class Client:
 
                 if 'status' in resp and 'data' in resp and 'usage' in resp:
                     if resp['status'] == 'success':
-                        return { 'data': resp['data'], 'usage': resp['usage'] }
+                        return { 'status': 'success', 'data': resp['data'], 'usage': resp['usage'] }
 
                 if 'developer_message' in resp:
                     message = resp['developer_message']
@@ -91,3 +92,37 @@ class Client:
             message = resp['developer_message']
 
         return { 'result': 'unknown', 'tags': [], 'message': message }
+
+    """
+        Search for people using the query parameters.
+    """
+    def people(self, query):
+        resp = request('/people', key=self.key, fields=query, method='GET')
+
+        if 'status' in resp:
+            if resp['status'] == 'success':
+                return { 'status': 'success', 'data': resp['data'] }
+
+        message = 'An unknown error ocurred'
+
+        if 'developer_message' in resp:
+            message = resp['developer_message']
+
+        return { 'status': 'failure', 'data': [], 'message': message }
+
+    """
+        Reveal will unlock people previews from "search".
+    """
+    def reveal(self, uuids):
+        resp = request('/people', key=self.key, payload={'uuids': uuids}, method='POST')
+
+        if 'status' in resp:
+            if resp['status'] == 'success':
+                return { 'status': 'success', 'data': resp['data'], 'usage': resp['usage'] }
+
+        message = 'An unknown error ocurred'
+
+        if 'developer_message' in resp:
+            message = resp['developer_message']
+
+        return { 'status': 'failure', 'data': [], 'message': message }
